6fc9e700732bb0c41030f5c4e072ebd7c1a05391
Marcela Bomfim <mbomfim@live.com>
2018-11-02 16:59:10 -0200

    Adicionado lin para fonts

--- src/theme/global.js
+++ src/theme/global.js
@@ -4,5 +4,10 @@ export default `
   body {
     background: ${theme.colors.white};
     color: ${theme.colors.black};
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3, h4 {
+    font-family: 'Quicksand', sans-serif;
   }
 `;
