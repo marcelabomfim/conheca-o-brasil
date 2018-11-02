0641b51bed9b873cb5a6e9259e76adbb80fb4ff8
Marcela Bomfim <mbomfim@live.com>
2018-11-02 16:59:32 -0200

    Criado componente de ícones

---
+++ src/components/Icon/styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.svg.attrs({
+  viewBox: '0 0 1024 1024'
+})`
+  width: 22px;
+  height: auto;
+  color: inherit;
+  display: inline-block;
+  vertical-align: middle;
+`;
