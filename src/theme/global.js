6ccdd51c7311e1d8bbf4c70b1f6b525f9bd2f9d9
Marcela Bomfim <mbomfim@live.com>
2018-11-04 11:12:51 -0200

    Update theme variables and global styles

--- src/theme/global.js
+++ src/theme/global.js
@@ -2,12 +2,25 @@ import theme from './variables';
 
 export default `
   body {
-    background: ${theme.colors.white};
-    color: ${theme.colors.black};
-    font-family: 'Open Sans', sans-serif;
+    color: ${theme.colors.text};
+    font-family: ${theme.font.text};
+
+    background: ${theme.colors.bg1};
+    background: -moz-linear-gradient(45deg, ${theme.colors.bg1} 30%, ${
+  theme.colors.bg2
+} 100%);
+    background: -webkit-linear-gradient(45deg, ${theme.colors.bg1} 30%,${
+  theme.colors.bg2
+} 100%);
+    background: linear-gradient(45deg, ${theme.colors.bg1} 30%,${
+  theme.colors.bg2
+} 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${
+      theme.colors.bg1
+    }', endColorstr='${theme.colors.bg2}',GradientType=1 );
   }
 
   h1, h2, h3, h4 {
-    font-family: 'Quicksand', sans-serif;
+    font-family: ${theme.font.title};
   }
 `;
--- src/theme/variables.js
+++ src/theme/variables.js
@@ -1,7 +1,10 @@
 export default {
   colors: {
-    white: '#fff',
-    black: '#000'
+    text: '#fff',
+    shadow: '#333333',
+    bg1: '#BB6BD9',
+    bg2: '#56CCF2',
+    primary: '#FF3E91'
   },
   spacing: {
     xsmall: '4px',
@@ -9,5 +12,9 @@ export default {
     base: '16px',
     large: '24px',
     xlarge: '32px'
+  },
+  font: {
+    text: '"Open Sans", sans-serif',
+    title: '"Quicksand", sans-serif'
   }
 };
