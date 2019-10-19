c7abd94c2a3c112df9e0e09df1d63005ba148c42
Luckened <luccafmsilva@gmail.com>
2019-10-19 17:46:48 -0300

    Adicionando funcionalidade para o seletor de estados

--- src/components/StatesSelect/index.jsx
+++ src/components/StatesSelect/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default ({ states, selected }) => (
-  <select>
+export default ({ states, selected, onClickOption }) => (
+  <select onClick={value => onClickOption(value)}>
     {states.map(state => (
       <option key={state.uf} value={state.uf} selected={selected === state.uf}>
         {state.name}
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -9,6 +9,7 @@ import Icon from 'components/Icon';
 import IconContainer from 'components/IconContainer';
 
 import { getStates, getRandomLocal } from 'services';
+import locals from 'assets/data/locals.json';
 
 export default class extends Component {
   state = {
@@ -32,13 +33,29 @@ export default class extends Component {
     });
   };
 
+  stateSelector = value => {
+    const filteredState = locals.filter(item => item.uf === value.target.value);
+
+    if (filteredState.length) {
+      const selectedLocal = {
+        ...filteredState[0],
+        imagem: require('../../' + filteredState[0].imagem)
+      };
+      this.setState({ local: selectedLocal });
+    }
+  };
+
   render() {
     const { states, local } = this.state;
 
     return (
       <Container justifyBetween>
         <Header />
-        <StatesSelect states={states} selected={local && local.uf} />
+        <StatesSelect
+          onClickOption={this.stateSelector}
+          states={states}
+          selected={local && local.uf}
+        />
         {local && <Local local={local} />}
         <IconContainer />
         <Button onClick={this.getNextLocal}>
